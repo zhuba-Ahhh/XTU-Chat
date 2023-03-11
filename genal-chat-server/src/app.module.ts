fe0ea1527dfc8686c25f134021ab6a69d6dfac72
zhuba-Ahhh <3477826311@qq.com>
2023-03-12 04:14:11 +0800

    docs: readme...

--- genal-chat-server/src/app.module.ts
+++ genal-chat-server/src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './modules/auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       port: 3306,
-      username: 'root',
+      username: 'chat',
       password: 'ywj88888888',
       database: 'chat',
       charset: "utf8mb4", // 设置chatset编码为utf8mb4
